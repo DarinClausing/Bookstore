5ae4e779f31d2b25e5c8f8fbd5a00528eff79f96
Darin Clausing <clausing.d@gmail.com>
2015-06-25 11:05:36 -0700

    Generating Books

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514203013) do
+ActiveRecord::Schema.define(version: 20150605160427) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "name",         limit: 255
+    t.string   "author",       limit: 255
+    t.text     "description",  limit: 65535
+    t.integer  "price",        limit: 4
+    t.boolean  "availability", limit: 1
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
